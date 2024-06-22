46b3f580c2f35ddeb9d69011b0bfdb97fac03e8b
Michael Wolf <susonicth@gmail.com>
2024-06-22 21:17:39 +0200

    removed file_list array allocation; fixed/improved read_line

--- csvtool.c
+++ csvtool.c
@@ -1,6 +1,9 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define UNIT_TEST 1
 
 #define NOT_SET NULL
 #define ARG_EQUALS(arg) (arg != NULL && strcmp(argv[i], arg) == 0)
@@ -10,7 +13,8 @@ void print_usage(FILE *fp) {
     fprintf(fp, "CSVTool V0.1\n\n");
     fprintf(fp, "usage csvtool [options] [file 1] .. [file n] \n\n");
     fprintf(fp, "options:\n");
-    fprintf(fp, "        -d, --delimiter <character> the delimiter to use (default ,)\n");
+    fprintf(fp, "        -                       stop processing arguments, extras are filenames\n");
+    fprintf(fp, "        -d, --delimiter <char>  the delimiter to use (default ,)\n");
     fprintf(fp, "\n");
 }
 
@@ -28,19 +32,7 @@ char use_stdin = 0;
 size_t minimum_read_size = 4096;
 
 char **input_files = NULL;
-size_t input_files_size = 16;
 size_t input_files_count = 0;
-
-// todo: check alloc error
-void add_file(char *file_name) {
-    if (input_files == NULL) {
-        input_files = malloc(sizeof(char *) * input_files_size);
-    } else if (input_files_count >= input_files_size) {
-        input_files_size *= 2;
-        input_files = realloc(input_files, sizeof(char *) * input_files_size);
-    }
-    input_files[input_files_count++] = file_name;
-}
 
 char *buffer = NULL;
 size_t buffer_size = 0;
@@ -48,55 +40,58 @@ char *buffer_pos = NULL;
 char *buffer_end = NULL;
 char eof = 0;
 
-char *fill_buffer(FILE *fp) {
-    int pos = buffer_pos - buffer;
-    int space_left = buffer_size - pos;
-    memcpy(buffer, buffer_pos, buffer_end - buffer_pos);
-    buffer_end = buffer + pos + fread(buffer, buffer_size, 1, fp);
+void fill_buffer(FILE *fp, char **current, char **line) {
+    int line_len = (*current - *line);
+    int space_left = buffer_size - line_len;
+    memcpy(buffer, *line, line_len);
     if (space_left < minimum_read_size) {  // todo: check alloc error
         buffer_size *= 2;
         buffer = realloc(buffer, buffer_size);
-        buffer_pos = buffer;
-        space_left = buffer_size - pos;
+        space_left = buffer_size - line_len;
     }
-    char *line = buffer + pos;
-    buffer_end = buffer + pos + fread(line, 1, space_left, fp);
-    return line;
+    *current = buffer + line_len;
+    *line = buffer;
+    buffer_end = buffer + line_len + fread(*current, 1, space_left, fp);
+}
+
+void read_line_init(FILE *fp) {
+    buffer_size = minimum_read_size * 2;
+    buffer = (char *)malloc(buffer_size);
+    buffer_pos = buffer;
+    int read = fread(buffer, 1, buffer_size, fp);
+    buffer_end = buffer + read;
 }
 
 char *read_line(FILE *fp) {
-    if (buffer == NULL) {
-        buffer_size = minimum_read_size * 2;
-        buffer = malloc(sizeof(buffer_size));
-        buffer_pos = buffer;
-        buffer_end = buffer + fread(buffer, 1, buffer_size, fp);
-    }
-
     char *line = buffer_pos;
     char *current = buffer_pos;
 
     if (buffer_pos >= buffer_end && feof(fp)) {
-        printf("EOF");
-        fflush(stdout);
         return NULL;
     }
 
     while (1) {
-        while (*current != '\r' && *current != '\n' && current <= buffer_end) {
+        while (*current != '\r' && *current != '\n' && current < buffer_end) {
             current++;
         }
-        if (current <= buffer_end) {
-            *current = 0;
+        if (current < buffer_end) {
             buffer_pos = current + 1;
-            if (*current == '\r' && current < buffer_end && *(current + 1) == '\n') {
-                buffer_pos++;
+            if (*current == '\r') {
+                if (current < buffer_end && *(current + 1) == '\n') {
+                    buffer_pos++;
+                } else {
+                    fill_buffer(fp, &current, &line);
+                    if (*(current + 1) == '\n') {
+                        buffer_pos++;
+                    }
+                }
             }
+            *current = 0;
             return line;
         } else {
-            current = fill_buffer(fp);
-            line = buffer;
+            fill_buffer(fp, &current, &line);
 
-            if (buffer_pos == buffer_end && feof(fp)) {
+            if (current == buffer_end && feof(fp)) {
                 *current = 0;
                 return line;
             }
@@ -108,16 +103,23 @@ void process(FILE *fp) {
     char *line;
     int i = 0;
     while ((line = read_line(fp)) != NULL) {
-        printf("%d: %s\n", i++, line);
-        fflush(stdout);
+        fwrite(line, strlen(line), 1, stdout);
+        fputc('\n', stdout);
     }
 }
 
 #ifndef UNIT_TEST
 int main(int argc, char **argv) {
-    for (int i = 1; i < argc; i++) {
+    for (int i = 1; i < argc && input_files == NULL; i++) {
         if (argv[i][0] != '-') {
-            add_file(argv[i]);
+            input_files = &argv[i];
+            input_files_count = argc - i;
+        } else if (IS_ARG("-", NOT_SET)) {
+            i++;
+            if (i < argc) {
+                input_files = &argv[i];
+                input_files_count = argc - i;
+            }
         } else if (IS_ARG("-h", "--help")) {
             print_usage(stdout);
             return 0;
@@ -125,10 +127,13 @@ int main(int argc, char **argv) {
             delimiter = get_arg_value("delimiter", ++i, argc, argv)[0];
         } else if (IS_ARG("-c", "--use_stdin")) {
             use_stdin = 1;
+        } else {
+            print_usage(stderr);
+            fprintf(stderr, "Error: unknown argument '%s'\n", argv[i]);
         }
     }
 
-    if (argc == 1 || (input_files_count == 0 && use_stdin == 0)) {
+    if (argc == 1 || (input_files == NULL && use_stdin == 0)) {
         print_usage(stderr);
         fprintf(stderr, "Error: either -c/--use_stdin or a filename has to be given as argument\n");
         return (1);
@@ -139,8 +144,11 @@ int main(int argc, char **argv) {
         process(stdin);
     } else {
         for (int i = 0; i < input_files_count; i++) {
-            // todo: check file open error
             FILE *fp = fopen(input_files[i], "rb");
+            assert(fp != NULL);
+            if (i == 0) {
+                read_line_init(fp);
+            }
             process(fp);
             fclose(fp);
         }
@@ -213,28 +221,20 @@ int _number_equals(size_t expected, size_t actual) {
     return 1;
 }
 
-void test_file_list_empty() {
-    _assert(input_files == NULL);
-    _assert(input_files_count == 0);
+int _is_NULL(void *ptr) {
+    if (ptr != NULL) {
+        printf("\nexpected NULL got \"%p\"\n", ptr);
+        return 0;
+    }
+    return 1;
 }
 
-void test_file_list_add() {
-    input_files_size = 2;
-
-    add_file("File 1");
-    _assert(_number_equals(1, input_files_count));
-    _assert(_number_equals(2, input_files_size));
-    _assert(_str_equals("File 1", input_files[0]));
-
-    add_file("File 2");
-    _assert(_number_equals(2, input_files_count));
-    _assert(_number_equals(2, input_files_size));
-    _assert(_str_equals("File 2", input_files[1]));
-
-    add_file("File 3");
-    _assert(_number_equals(3, input_files_count));
-    _assert(_number_equals(4, input_files_size));
-    _assert(_str_equals("File 3", input_files[2]));
+int _is_not_NULL(void *ptr) {
+    if (ptr == NULL) {
+        printf("\nexpected not NULL\n");
+        return 0;
+    }
+    return 1;
 }
 
 void test_is_arg() {
@@ -248,14 +248,45 @@ void test_is_arg() {
     _assert(IS_ARG("-d", "--delimiter"));
     _assert(IS_ARG(NOT_SET, "--delimiter"));
 
-    i = 3;
+    i = 2;
     _assert_not(IS_ARG("-d", "--delimiter"));
 }
 
+void _write_lines_to_file(char *file_name, char *eol, char **lines) {
+    size_t eol_size = strlen(eol);
+    FILE *fp = fopen(file_name, "wb");
+    assert(fp != NULL);
+    while (*lines != NULL) {
+        fwrite(*lines, strlen(*lines), 1, fp);
+        fwrite(eol, eol_size, 1, fp);
+        lines++;
+    }
+    fclose(fp);
+}
+
+void test_readLine() {
+    char *TEST_FILE_NAME = "./test/lineRederTest.txt";
+    char *test_lines[] = {"Test", "Test2", "Test3", "much longer line that causes the buffer to grow multiple times", "Test4", NULL};
+    _write_lines_to_file(TEST_FILE_NAME, "\n", test_lines);
+
+    FILE *fp = fopen(TEST_FILE_NAME, "rb");
+    assert(fp != NULL);
+
+    minimum_read_size = 2;  // tet extra low to trigger multiple buffer size doublings
+    read_line_init(fp);
+
+    char **line = test_lines;
+    while (*line != NULL) {
+        _assert(_str_equals(*line, read_line(fp)));
+        line++;
+    }
+    _assert(_is_NULL(read_line(fp)));
+    fclose(fp);
+}
+
 int main(int argc, char **argv) {
-    _verify(test_file_list_empty);
-    _verify(test_file_list_add);
     _verify(test_is_arg);
+    _verify(test_readLine);
 
     printf("\n");
     printf(" Tests run %d\n", tests_run);
